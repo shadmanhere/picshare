d2dcaf4dbb5f3169d4efd9cfb3c1e79bfa6d7b9e
Shadman Ali <shadman.ali@live.co.uk>
2022-07-14 15:44:18 +0530

    database connection and .env setup

--- server/index.js
+++ server/index.js
@@ -2,3 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import 'dotenv/config'
+
+const app = express();
+
+app.use(bodyParser.json({limit:"32mb", extended:true}))
+app.use(bodyParser.urlencoded({ limit:"32mb", extended:true}))
+app.use(cors())
+
+const DB_CONNECTION_URL = process.env.DB_CONNECTION_URL
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(DB_CONNECTION_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(() => app.listen(PORT, ()=>console.log(`Server running on port:${PORT}`)))
+  .catch(err => console.log(err.message))
