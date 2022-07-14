68cd13ea8341f1253077f14360f808c499202ef5
Shadman Ali <shadman.ali@live.co.uk>
2022-07-14 20:40:13 +0530

    style added to App component

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
 import travel from './images/travel.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import './styles.css'
 
 const App = () => {
   return (
     <Container maxWidth="lg">
-        <AppBar position="static" color="inherit">
-            <Typography variant="h2" align="center">Travel</Typography>
-            <img src={travel} alt="travel" height="60" />
+        <AppBar id="appBar" position="static" color="inherit">
+            <Typography id="heading" variant="h2" align="center">Travel</Typography>
+            <img id="image" src={travel} alt="travel" width="60" height="60" />
         </AppBar>
         <Grow in>
             <Container>
