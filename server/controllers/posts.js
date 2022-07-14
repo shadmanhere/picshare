a5a8c482b3cb04dc16dc33f9719bcaa3d4949bc9
Shadman Ali <shadman.ali@live.co.uk>
2022-07-14 18:11:26 +0530

    update: controller and route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,23 @@
-export const getPosts = (req, res) => {
-    res.send('Post Works!')
+import PostMessage from "../models/postMessage.js"
+
+export const getPosts = async (req, res) => {
+    try{
+        const postMessage = await PostMessage.find()
+        res.status(200).json(postMessage);
+    } catch(err){
+        res.status(404).json({message: err.message});
+    }
+}
+
+export const createPost = async (req, res) => {
+    const body = req.body;
+
+    const newPost = new PostMessage(body);
+
+    try{
+        await newPost.save()
+        res.status(201).json(newPost);
+    } catch(err) {
+        res.status(409).json({message: err.message});
+    }
 }
