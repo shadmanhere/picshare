337cf9fcad4f6821d033a78a03479cb5a820b4cd
Shadman Ali <shadman.ali@live.co.uk>
2022-07-14 19:23:49 +0530

    update client: mui added, image added and all components added in App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import travel from './images/travel.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+        <AppBar position="static" color="inherit">
+            <Typography variant="h2" align="center">Travel</Typography>
+            <img src={travel} alt="travel" height="60" />
+        </AppBar>
+        <Grow in>
+            <Container>
+                <Grid container justify="space-between" alignItems="stretch" spacing={4}>
+                    <Grid item xw={12} sm={7}>
+                        <Posts />
+                    </Grid>
+                    <Grid item xw={12} sm={4}>
+                        <Form />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+    </Container>
   );
 };
 
