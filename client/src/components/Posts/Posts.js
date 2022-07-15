5c5af8e03faef7a2ef6d5444b3b207e2b416c0bf
Shadman Ali <shadman.ali@live.co.uk>
2022-07-15 19:10:41 +0530

    update: client: posts component

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,17 +1,22 @@
 import React from "react";
 import Post from "./Post/Post"
 import {useSelector} from "react-redux";
+import { Grid, CircularProgress } from '@mui/material'
 
 const Posts = () => {
   const posts = useSelector((state) => state.posts)
 
   console.log(posts);
   return (
-    <>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </>
+    !posts.length ? <CircularProgress /> : 
+    <Grid container alignItems="stretch" spacing={3}>
+      {posts.map(post => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))
+      }
+    </Grid>
   );
 };
 
