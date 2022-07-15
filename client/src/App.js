51a157bf063409ec64cc8b5efb09ddd97aa23367
Shadman Ali <shadman.ali@live.co.uk>
2022-07-15 15:10:16 +0530

    update client: redux, react-reduc, reducers, action implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,44 @@
-import React from "react";
-import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
-import travel from './images/travel.png';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import './styles.css'
+import React, { useEffect } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts";
+
+import travel from "./images/travel.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import "./styles.css";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
   return (
     <Container maxWidth="lg">
-        <AppBar id="appBar" position="static" color="inherit">
-            <Typography id="heading" variant="h2" align="center">Travel</Typography>
-            <img id="image" src={travel} alt="travel" width="60" height="60" />
-        </AppBar>
-        <Grow in>
-            <Container>
-                <Grid container justify="space-between" alignItems="stretch" spacing={4}>
-                    <Grid item xw={12} sm={7}>
-                        <Posts />
-                    </Grid>
-                    <Grid item xw={12} sm={4}>
-                        <Form />
-                    </Grid>
-                </Grid>
-            </Container>
-        </Grow>
+      <AppBar id="appBar" position="static" color="inherit">
+        <Typography id="heading" variant="h2" align="center">
+          Travel
+        </Typography>
+        <img id="image" src={travel} alt="travel" width="60" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={4}
+          >
+            <Grid item xw={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xw={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
---
+++ client/src/actions/posts.js
@@ -0,0 +1,7 @@
+import * as api from '../api';
+
+//Action Creator;
+const getPosts = () => (dispatch) => {
+    const action = {type:"FETCH_ALL", payload:[]}
+    return action;
+}
