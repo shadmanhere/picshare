db0e6993ae0c0eb29a8da588803e6eb60688d628
Shadman Ali <shadman.ali@live.co.uk>
2022-07-15 15:56:52 +0530

    update client: form component

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button, Typography, Paper } from "@mui/material";
 
 const Form = () => {
-    return <h1>Form</h1>;
-}
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+  const handleSubmit = () => {};
+  return (
+    <Paper>
+      <form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Typography variant="h6"> Creating a Post</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+      </form>
+    </Paper>
+  );
+};
 
 export default Form;
