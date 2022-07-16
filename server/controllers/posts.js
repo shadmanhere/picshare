89173f4a4f98ace3ad64c414df274aff8d3ab74b
Shadman Ali <shadman.ali@live.co.uk>
2022-07-16 12:43:30 +0530

    server: added updatePost functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -21,3 +21,13 @@ export const createPost = async (req, res) => {
         res.status(409).json({message: err.message});
     }
 }
+
+export const updatePost = async (req, res) => {
+    // id renamed to _id
+    const { id: _id } = req.params
+
+    const post = req.body;
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id was found');
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new:true})
+    res.json(updatedPost);
+}
